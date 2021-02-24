764676ba591e63f93320b10dbdc60b805a2f609f
yubinkim <kimu2371@naver.com>
2021-02-24 16:54:50 +0900

    add sth

--- app/src/main/java/isbean/android_practice/android_test/MainActivity.kt
+++ app/src/main/java/isbean/android_practice/android_test/MainActivity.kt
@@ -2,10 +2,16 @@ package isbean.android_practice.android_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import isbean.android_practice.android_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //branch : gitHub
+        //process : 0
     }
 }
