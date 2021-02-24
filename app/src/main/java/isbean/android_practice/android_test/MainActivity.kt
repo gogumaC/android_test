f42444c26254ce1282a67f28884ec38c186df5a3
yubinkim <kimu2371@naver.com>
2021-02-24 16:56:03 +0900

    add sth

--- app/src/main/java/isbean/android_practice/android_test/MainActivity.kt
+++ app/src/main/java/isbean/android_practice/android_test/MainActivity.kt
@@ -9,9 +9,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        //branch : gitHub
-        //process : 1
+        //branch : gitHub_rebase
+        //process:1
     }
 }
